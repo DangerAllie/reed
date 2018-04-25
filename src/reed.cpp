a7179948ed7a7bc7bdddb310fd849b74a31153f2
Allison Bianchi <allieb.github@gmail.com>
2018-04-25 16:57:03 -0700

    reed console stub

--- src/reed.cpp
+++ src/reed.cpp
@@ -4,8 +4,37 @@
 //
 // See LICENSE for use and distribution
 
+#include <iostream>
+#include <string>
+
+#include <stdio.h>
+
+void eventLoop()
+{
+    std::string cmd;
+
+    while (1) {
+        printf("reed@ ");
+        std::getline(std::cin, cmd);
+
+        if (cmd == "q" or cmd == "quit") {
+            return;
+        }
+        else if (cmd == "about") {
+            printf("\n");
+            printf("reed is a ridiculously extensible editor\n");
+            printf("\n");
+        }
+        else if (not cmd.empty()) {
+            printf("Unknown: '%s'\n", cmd.c_str());
+            printf("\n");
+        }
+    }
+}
+
 int main(int argc, char** argv)
 {
+    eventLoop();
     return 0;
 }
 
